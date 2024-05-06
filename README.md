ecf9e9e7150b171d735ba6762c371b64b0b5248a
tovitu <tovitusaikou@gmail.com>
2024-05-05 21:01:30 -0500

    final codes uploaded

--- README.md
+++ README.md
@@ -1,3 +1,12 @@
 # Open Sleep-Wake Scorer
 
-This project is dedicated to building a machine-learning based classifier for sleep-wake classification for mouse model of alzheimer's disease. It is an ongoing effort to achieve a satisfiable performance with balanced accuracy across 3 classes. More results are pending.
+This project is dedicated to building a machine-learning based classifier for sleep-wake classification for mouse model of alzheimer's disease. It is an ongoing effort to achieve a satisfiable performance with balanced accuracy across 3 classes.
+
+We implemented 3 machine learning models and 2 deep learning models for sleep staging on our collected dataset. We report the results in our written report. For reimplementation, please see the corresponding notebook to find the our pipelines. cnn_transformer.ipynb contains the cnn+transformer model proposed in our report. SVM_RF.ipynb contains the support vector machine and random forest models as the baseline. The multinomial_logitic_regression.R is the R script for multinomial regression. 
+
+ Due to the size the dataset, the data used by these pipelines are not included in this repository. We therefore share the data using google links:
+
+ raw data: https://drive.google.com/file/d/1_qGBp_afq7QhovnS-PD1F91FSF4dkeMH/view?usp=drive_link
+ processed data: https://drive.google.com/file/d/1vKHwSEf8NdUHPI6p1rAffCSR82Xv9Yzh/view?usp=drive_link
+
+Our notebooks contain the necessary codes to replicate the confusion matrices presented in the report. Simply downloading the dataset and running the notebook should be sufficient.
--- src/models.py
+++ src/models.py
@@ -63,3 +63,76 @@ class ClassDecoder(nn.Module):
         x = x.unsqueeze(1)
         encoded = self.encoder.encoder(x)
         return self.net(encoded)
+
+
+class ConvNet(nn.Module):
+    def __init__(self):
+        super(ConvNet, self).__init__()
+
+        self.encode = nn.Sequential(
+            nn.Conv1d(1, 16, 5),
+            nn.BatchNorm1d(16),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(2),
+            nn.Conv1d(16, 32, 5),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(2),
+            nn.Conv1d(32, 64, 5),
+            nn.BatchNorm1d(64),
+            nn.LeakyReLU(),
+            nn.MaxPool1d(2),
+        )
+
+        self.label_head = nn.Sequential(
+            nn.Linear(64 * 246, 128),
+            nn.BatchNorm1d(128),
+            nn.LeakyReLU(),
+            nn.Linear(128, 32),
+            nn.BatchNorm1d(32),
+            nn.LeakyReLU(),
+            nn.Linear(32, 3),
+        )
+
+    def forward(self, x):
+        x = self.encode(x)
+        x = x.flatten(start_dim=1)
+        x = self.label_head(x)
+        return x
+
+
+class AttenNet(nn.Module):
+    def __init__(self, hidden_size, num_classes, classifier, num_layers=4):
+        super(AttenNet, self).__init__()
+
+        self.seq_embedding = classifier
+
+        self.upsample = nn.Linear(3, hidden_size)
+
+        self.positional_embedding = nn.Parameter(
+            torch.randn(seq_len, hidden_size) / hidden_size**0.5
+        )
+
+        encoder_layer = nn.TransformerEncoderLayer(
+            d_model=hidden_size,
+            nhead=4,
+            dim_feedforward=hidden_size * 4,
+            activation=nn.GELU(),
+            batch_first=True,
+        )
+        self.transformer = nn.TransformerEncoder(encoder_layer, num_layers=num_layers)
+
+        self.classifier = nn.Linear(hidden_size, num_classes)
+
+    def forward(self, x):
+        logit = []
+        for i in range(x.size(1)):
+            logit.append(self.seq_embedding(x[:, [i]]))
+        x = torch.stack(logit, dim=1)
+        # x = F.softmax(x, dim=-1)
+        x = self.upsample(x)
+        x = x.view(x.size(0), seq_len, -1)
+        x = x + self.positional_embedding[:seq_len].unsqueeze(0)
+        x = self.transformer(x)
+        x = self.classifier(x)
+        return x
